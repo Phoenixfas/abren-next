2f6aab9f74bac1d39cd63ead531908b3101f0452
Fasika Dagnachew <fasika1515@gmail.com>
2024-11-21 13:10:45 +0300

    commit

--- app/en/Marquee.tsx
+++ app/en/Marquee.tsx
@@ -4,7 +4,7 @@ export default function Marquee() {
   return (
     <div className="marquee w-full h-20 flex items-center bg-[#54b3ba]">
         <div className="marquee-content text-3xl">
-            <span>Our most important focus areas in the improvement work are patient satisfaction</span> &#8226; 
+            <span>Our most important focus areas in the improvement work are</span> &#8226; 
             <span>Patient satisfaction</span> &#8226; 
             <span>Efficiency</span> &#8226; 
             <span>Safety</span> &#8226; 
